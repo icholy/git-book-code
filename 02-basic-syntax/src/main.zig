9d99b1c2ceada16ad00b5870af078f50412c2e27
Ilia Choly <ilia.choly@gmail.com>
2024-10-26 19:11:37 -0400

    split up

--- 02-basic-syntax/src/main.zig
+++ 02-basic-syntax/src/main.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 
-pub fn main() !void {
+pub fn main() !void {}
+
+pub fn variables() !void {
     // you can use 'undefined' to avoid specifying a default value
     var x: i32 = undefined;
-    std.debug.print("Undefined i32: {d}\n", .{x});
+    try std.debug.print("Undefined i32: {d}\n", .{x});
     x = 1;
 
     // you can assign to _ to avoid the unused local compiler error
