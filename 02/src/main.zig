8a8cba7c3b48e9a6d146e41d5907348e1dcdb6b4
Ilia Choly <ilia.choly@gmail.com>
2024-10-27 10:01:27 -0400

    struct

--- 02/src/main.zig
+++ 02/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {}
 
@@ -91,4 +92,23 @@ test "cannot mutate argument" {
     var x: u32 = 2;
     add2(&x);
     std.debug.print("{d}\n", .{x});
+}
+
+const User = struct {
+    id: u64,
+    name: []const u8,
+    email: []const u8,
+
+    pub fn init(id: u64, name: []const u8, email: []const u8) User {
+        return User{ .id = id, .name = name, .email = email };
+    }
+
+    pub fn print_name(self: User) !void {
+        try stdout.print("{s}\n", .{self.name});
+    }
+};
+
+test "struct" {
+    const u = User.init(123, "Ilia Choly", "ilia.choly@gmail.com");
+    try u.print_name();
 }
