06755774d3212f80574fcce8359e604d86857e34
Ilia Choly <ilia.choly@gmail.com>
2024-10-28 21:48:31 -0400

    first

--- 03/src/main.zig
+++ 03/src/main.zig
@@ -1,24 +1,15 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() !void {}
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+fn input_length(input: []const u8) usize {
+    return input.len;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "known at compile time" {
+    const name = "Ilia";
+    const array = [_]u8{ 1, 2, 3, 4 };
+    _ = name;
+    _ = array;
 }
