1a152848d583d83b48c1099b3dd634549d569e84
Ilia Choly <ilia.choly@gmail.com>
2024-11-02 22:03:51 -0400

    lots of changes

--- 05/src/main.zig
+++ 05/src/main.zig
@@ -3,31 +3,33 @@ const stdout = std.io.getStdOut().writer();
 const Ast = std.zig.Ast;
 
 const LaunchConfiguration = struct {
-    name: *const []u8,
-    type: *const []u8,
-    request: *const []u8,
-    program: *const []u8,
-    cwd: *const []u8,
-    args: *const []u8,
+    name: ?[]const u8 = null,
+    type: ?[]const u8 = null,
+    request: ?[]const u8 = null,
+    program: ?[]const u8 = null,
+    cwd: ?[]const u8 = null,
+    args: ?[]const []const u8 = null,
 };
 
 const LaunchJSON = struct {
-    Version: *const []u8,
+    version: []const u8,
+    configurations: []LaunchConfiguration,
 };
 
 pub fn main() !void {
+    const filename = "./src/main.zig";
     // setup allocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
     // read source file
-    var file = try std.fs.cwd().openFile("./src/main.zig", .{});
+    var file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
     const source = try file.readToEndAllocOptions(allocator, std.math.maxInt(usize), null, @alignOf(u8), 0);
 
     // find the test names and create configurations for them
-    var configurations = std.ArrayList(LaunchConfiguration).init(arena);
+    var configurations = std.ArrayList(LaunchConfiguration).init(allocator);
     var tree = try Ast.parse(allocator, source, .zig);
     const tags = tree.nodes.items(.tag);
     const datas = tree.nodes.items(.data);
@@ -37,11 +39,74 @@ pub fn main() !void {
         }
         const data = datas[index];
         const name = tree.tokenSlice(data.lhs);
+
+        // remove quotes
+        var unquoted = name;
+        if (std.mem.startsWith(u8, unquoted, "\"")) {
+            unquoted = unquoted[1..];
+        }
+        if (std.mem.endsWith(u8, unquoted, "\"")) {
+            unquoted = unquoted[0 .. unquoted.len - 1];
+        }
+
+        // bin name
+        var bin_name = try allocator.dupe(u8, unquoted);
+        for (bin_name, 0..) |c, i| {
+            bin_name[i] = switch (c) {
+                'a'...'z', 'A'...'Z', '0'...'9' => c,
+                else => '_',
+            };
+        }
+
+        // compile the test binary
+        const cmd = &[_][]const u8{
+            "zig",
+            "test",
+            filename,
+            "--test-filter",
+            unquoted,
+            try std.fmt.allocPrint(allocator, "-femit-bin=./zig-cache/tmp/{s}", .{bin_name}),
+        };
+        var child = std.process.Child.init(cmd, allocator);
+        child.stdout_behavior = .Pipe;
+        child.stdout_behavior = .Pipe;
+        child.stderr_behavior = .Pipe;
+
+        const term = try child.spawnAndWait();
+        switch (term) {
+            .Exited => |code| {
+                if (code != 0) {
+                    const formatted_cmd = try std.mem.join(allocator, " ", cmd);
+                    std.debug.print("failed to build test: {s}\n", .{formatted_cmd});
+                    return error.TestBuildFailed;
+                }
+            },
+            else => {},
+        }
+
+        // add launch configuration
         try configurations.append(.{
-            .name = try std.fmt.allocPrint(allocator, "Run Test ({s})", .{name}),
+            .name = try std.fmt.allocPrint(allocator, "Run Test: {s}", .{name}),
+            .type = "lldb",
+            .request = "launch",
+            .cwd = "${workspaceFolder}",
+            .program = try std.fmt.allocPrint(allocator, "${{workspaceFolder}}/zig-cache/tmp/{s}", .{bin_name}),
         });
     }
-    std.debug.print("{any}\n", .{try configurations.toOwnedSlice()});
+
+    const launch = LaunchJSON{
+        .version = "0.2.0",
+        .configurations = try configurations.toOwnedSlice(),
+    };
+
+    try std.json.stringify(launch, .{ .whitespace = .indent_tab, .emit_null_optional_fields = false }, stdout);
+}
+
+fn is_valid_filepath_char(ch: u8) bool {
+    return switch (ch) {
+        'a'...'z', 'A'...'Z', '0'...'9', '_' => true,
+        else => false,
+    };
 }
 
 test "foo" {
